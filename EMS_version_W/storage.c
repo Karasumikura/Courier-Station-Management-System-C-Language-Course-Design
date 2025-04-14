f6ec246abedbde932adae54d92e5ffee00e72a5d
AZ48661 <yswa@qq.com>
2025-04-15 00:16:37 +0800

    数据初始化

---
+++ EMS_version_W/storage.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include "storage.h"
+#include "user.h"
+#include "package.h"
+#include "shelf.h"
+#include "transaction.h"
+
+// 初始化数据存储
+void initDataStorage() {
+	// 确保数据目录存在
+	// 实际应用中可能需要创建目录
+}
+
+// 保存所有数据
+void saveAllData() {
+	saveUsersToFile("users.txt");
+	savePackages_File("packages.txt");
+	saveShelvesToFile("shelves.txt");
+	saveTransactionsToFile("transactions.txt");
+}
+
+// 加载所有数据
+void loadAllData() {
+	loadUsersFromFile("users.txt");
+	loadPackagesFromFile("packages.txt");
+	loadShelvesFromFile("shelves.txt");
+	loadTransactionsFromFile("transactions.txt");
+}
